dbaa280779652f2782b8b0fd5197fdef4276256a
kamisamacz <kamisama.cz@gmail.com>
2024-11-12 21:07:36 +0100

    prni pokus obejkty patrik stavy

--- hodiny/objekty.py
+++ hodiny/objekty.py
@@ -15,8 +15,10 @@ class people:
         self.stav = "po rozschodu"
         print(f"{self.jmeno} mel s ni problem, a je {self.stav}")
         print()
-
+    def stari(self):
+        self.vek = self.vek * 2
 patrik = people("Patrik",169, 98, 34, "nezadany")
+magda = people("Magda",169, 58, 20, "nezadany")
 
 print("Patrik:")
 print(patrik.vyska, " ",patrik.vaha, "",patrik.vek, "",patrik.stav)
@@ -25,3 +27,26 @@ patrik.rande()
 print("Patrik:")
 print(patrik.vyska, " ",patrik.vaha, "",patrik.vek, "",patrik.stav)
 patrik.nerande()
+if patrik.stav == magda.stav:
+    jebacka = magda.vaha + patrik.vaha
+    print("splnili podminku por jebacku")
+
+else:
+    print("nejebalo se")
+
+a = patrik.vek + patrik.vek
+
+print("dvojnasobny vek bude: ",a)
+magda.rande()
+patrik.rande()
+if patrik.stav == magda.stav:
+    jebacka = magda.vaha + patrik.vaha
+    print("splnili podminku por jebacku")
+
+else:
+    print("nejebalo se")
+print("patrikova postel unesla: ", jebacka)
+rozdil = abs(patrik.vek - magda.vek)
+print("veky rozdil je:",rozdil)
+if rozdil >10:
+    print("najdi si uz sratsi vole")
